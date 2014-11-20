0a5846b178ee167c5ae24ed224922a3f15e93cfb
yakasan <mehdichilla33@gmail.com>
2014-11-20 11:31:40 +0100

    modif

--- model/class.create.php
+++ model/class.create.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once("model/class.model.php");
+
+function getByIdAndJoin($id){
+		$query=parent::$connexion->prepare('select * FROM message INNER JOIN utilisateur on message.id_utilisateur=utilisateur.id WHERE message.id = :id;');
+		$query->bindParam(":id", $id);
+		$query->execute();
+		return $query->fetchAll(PDO::FETCH_ASSOC);
+	}
