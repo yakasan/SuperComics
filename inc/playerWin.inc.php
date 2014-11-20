0cba1f3d596dc8d9e838f692042324ec0d4cbb7e
seto <sylvain.lavayssiere@gmail.com>
2014-11-20 16:08:50 +0100

    modif player win via bdd player win avec jointure etc

--- inc/playerWin.inc.php
+++ inc/playerWin.inc.php
@@ -5,12 +5,25 @@
     </tr>
     
     <?php
+    
+    /*****
+    * réalisation d'une jointure de table : 
+    * FROM table ou les données sont récup -> p représente propriétaire
+    * INNER JOIN table source -> i représente initiale
+    * On -> p.id = id propriétaire /-----prend la valeur de------ / i.id->id initiale
+    *****/
+    
     $connexion = new PDO('mysql:host=localhost; dbname=SuperComics', 'stagiaire', 'stagiaire');
-    $sql = 'SELECT * FROM player';
+    $sql = 'SELECT * 
+            FROM player_win p
+            INNER JOIN player i
+            ON p.id=i.id';
     $req = $connexion->query($sql);
+    
     //print_r($req);
+    
     while($row = $req->fetch()){
-        echo "<tr><td>".$row['pseudo']."</td><td>".$row['id']."</td></tr>";
+        echo "<tr><td>".$row['pseudo']."</td><td>".$row['date']."</td></tr>";
     }
     ?>
 </table>
