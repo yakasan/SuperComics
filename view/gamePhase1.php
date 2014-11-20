3d8cfd9bd5845840d8308898de2d1e211e118e45
repgarent <repgarent@gmail.com>
2014-11-20 11:53:42 +0100

    modif css

---
+++ view/gamePhase1.php
@@ -0,0 +1,27 @@
+<?php include("header.php");?>
+<body>
+    <header> 
+        <h1>A qui le tour ?</h1>
+        </br>
+    </header>
+
+
+    <section>
+        <aside class="player">
+            joueur 1
+
+        </aside>
+        <aside class="vs">
+            VS
+        </aside>
+        <aside class="challenger">
+            joueur 2
+        </aside>
+
+        <a><input type="submit" value="DEFIER!" class="defyButton, typo" ></a>
+
+    </section>
+   
+        
+</body>
+<?php include("footer.php");?>
