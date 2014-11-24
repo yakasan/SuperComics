04d1f58e992d0fd79786ee08458910682b8c9f00
yakasan <mehdichilla33@gmail.com>
2014-11-24 11:49:50 +0100

    modif

--- process/seePlayer.php
+++ process/seePlayer.php
@@ -37,23 +37,4 @@
 
 	header('location:../controller/createController.php');
 
-	    $dbh->exec("SET CHARACTER SET utf8");
-
-	    /*
-	     * je récupère le type et le nom du formulaire fictif : 
-	     */
-	    $pseudo = $dbh->quote($_POST['select']); 
-
-	    $checked = $dbh -> exec("UPDATE player SET checked='1'");
-	    
-	     /* Insertion d'une ligne dans MySQL
-	    $count = $dbh->exec("INSERT INTO player(pseudo, compteur) VALUES ($pseudo, $compteur)");
-*/
-	}
-}else{
-
-	header('location:../controller/createController.php');}
-   
-header('location:../controller/createController.php');
-
 ?>
