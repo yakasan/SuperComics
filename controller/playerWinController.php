530c4d5fb8a8ffbee05599644ab0aa94409eaba4
seto <sylvain.lavayssiere@gmail.com>
2014-11-24 09:58:56 +0100

    pourquoi pas

--- controller/playerWinController.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include("/SuperComics/model/class.model.php");
-    $result = $connexion->query("SELECT * FROM player");
-    $playerWin=$result->fetchAll(PDO::FETCH_ASSOC);
-    include("/SuperComics/index.tpl");
-
-?>
--- inc/connexion.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connexion = new PDO('mysql:host=localhost; dbname=SuperComics', 'stagiaire', 'stagiaire');
-    ?>
-
