6f07471c035b5858afd8c21c34b4dbbcaa2a26cd
yakasan <mehdichilla33@gmail.com>
2014-11-24 09:47:27 +0100

    jgfdlclj;lb ukhgvljuyfvluyhgvluyfvmiyfv

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,12 +4,6 @@
 			<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 			<meta content="utf-8" http-equiv="encoding">
 			<title>Super Comics</title>
-<<<<<<< HEAD
-			<link rel="stylesheet" href="/SuperComics/css/bootstrap.css">
-			<link rel="stylesheet" href="/SuperComics/css/style.css">
-			<link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
-=======
->>>>>>> f4b57947cfa110175bf06b353035d491f4c0c521
 			<script src="js/jquery-2.1.1.js"></script>
 			<script src="js/jquery/jquery-ui.js"></script>
 			<script src="js/bootstrap.js"></script>
