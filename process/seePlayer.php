c26a19bbdbc4263f87aaecee8f1757b7dd0618a0
seto <sylvain.lavayssiere@gmail.com>
2014-11-24 11:53:00 +0100

    modif see player again

--- process/seePlayer.php
+++ process/seePlayer.php
@@ -9,50 +9,31 @@
 	    $dbh = new PDO('mysql:host='.$host .';dbname='.$dbname, $user, $password );
 	    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	//print_r($_POST['checked']);
+//print_r($_POST['checked']);
+
+
+if(isset($_POST['checked']))
+{    
+	//recupérer ces valeurs dans un array
+	$tabChecked = $_POST['checked'];
 	
+	//print_r($tabChecked);
 	
-	if(isset($_POST['checked']))
-	{    
-		//recupérer ces valeurs dans un array
-		$tabChecked = $_POST['checked'];
-		
-		//print_r($tabChecked);
-		
-		foreach ($tabChecked as $pseudo) 
-		{
-			$pseudoPlayer = addslashes($pseudo);
-			$req = $dbh->prepare("UPDATE player 
-			            SET checked ='1'
-						WHERE pseudo = :pseudoPlayer");
-			$req->execute(array(
-			'pseudoPlayer' => $pseudoPlayer
-			));
-		}
-	}	
-	else
-	{	
-        header('location:../controller/createController.php');
+	foreach ($tabChecked as $pseudo) 
+	{
+		$pseudoPlayer = addslashes($pseudo);
+		$req = $dbh->prepare("UPDATE player 
+		            SET checked ='1'
+					WHERE pseudo = :pseudoPlayer");
+		$req->execute(array(
+		'pseudoPlayer' => $pseudoPlayer
+		));
 	}
-
-	header('location:../controller/createController.php');
-
-	    $dbh->exec("SET CHARACTER SET utf8");
-
-	    /*
-	     * je récupère le type et le nom du formulaire fictif : 
-	     */
-	    $pseudo = $dbh->quote($_POST['select']); 
-
-	    $checked = $dbh -> exec("UPDATE player SET checked='1'");
-	    
-	     /* Insertion d'une ligne dans MySQL
-	    $count = $dbh->exec("INSERT INTO player(pseudo, compteur) VALUES ($pseudo, $compteur)");
-*/
-	}
-}else{
-
-	header('location:../controller/createController.php');}
+}	
+else
+{	
+    header('location:../controller/createController.php');
+}
    
 header('location:../controller/createController.php');
 
